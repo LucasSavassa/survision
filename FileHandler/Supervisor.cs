b3494df1e9448d299eae7f36b760f09b8660ade0
Lucas Pires Savassa <lucaspsavassa@gmail.com>
2024-07-03 10:24:30 -0300

    Implement monitoring of processing folder

--- FileHandler/Supervisor.cs
+++ FileHandler/Supervisor.cs
@@ -13,6 +13,7 @@ namespace FileHandler
         private const int _delay = 5000;
         private const string _queuePath = @"C:\Users\lucas\OneDrive\Desktop\Survision\Queue";
         private const string _processingPath = @"C:\Users\lucas\OneDrive\Desktop\Survision\Processing";
+        private const string _processedPath = @"C:\Users\lucas\OneDrive\Desktop\Survision\Processed";
         private const string _binPath = @"C:\Users\lucas\OneDrive\Desktop\Survision\Bin";
         private const string _folderNamePattern = @"\bsurgery-[0-9]{1,11}\b";
 
@@ -33,6 +34,7 @@ namespace FileHandler
                 }
 
                 MonitorQueueFolder();
+                MonitorProcessingFolder();
 
                 await Task.Delay(_delay, stoppingToken);
             }
@@ -73,10 +75,10 @@ namespace FileHandler
                 if (_logger.IsEnabled(LogLevel.Information))
                 {
                     _logger.LogInformation("Creating queue folder: {queuePath}", _queuePath);
-            }
+                }
 
                 Directory.CreateDirectory(_queuePath);
-        }
+            }
         }
 
         private void ProcessFileAtQueue(string file)
@@ -151,12 +153,12 @@ namespace FileHandler
                 if (_logger.IsEnabled(LogLevel.Information))
                 {
                     _logger.LogInformation("Moving file to bin folder: {file}", file);
-        }
+                }
 
                 File.Move(file, _binPath);
             }
             else
-        {
+            {
                 ZipFile.ExtractToDirectory(file, destination);
                 File.Delete(file);
             }
@@ -198,6 +200,162 @@ namespace FileHandler
             }
 
             return hasMetadata && hasImage;
+        }
+
+        private void MonitorProcessingFolder()
+        {
+            if (Directory.Exists(_processingPath))
+            {
+                string[] entries = Directory.GetFileSystemEntries(_processingPath);
+                if (entries.Length > 0)
+                {
+                    foreach (var entry in entries)
+                    {
+                        ProcessEntryAtProcessing(entry);
+                    }
+                }
+                else
+                {
+                    if (_logger.IsEnabled(LogLevel.Error))
+                    {
+                        _logger.LogError("No entries to process.");
+                    }
+                }
+            }
+            else
+            {
+                if (_logger.IsEnabled(LogLevel.Error))
+                {
+                    _logger.LogError("Processing folder does not exist.");
+                }
+
+                if (_logger.IsEnabled(LogLevel.Information))
+                {
+                    _logger.LogInformation("Creating processing folder: {processingPath}", _processingPath);
+                }
+
+                Directory.CreateDirectory(_processingPath);
+            }
+        }
+
+        private void ProcessEntryAtProcessing(string entry)
+        {
+            string folderName = Path.GetFileName(entry);
+
+            if(_logger.IsEnabled(LogLevel.Information))
+            {
+                _logger.LogInformation("Processing entry: {entry}", entry);
+            }
+
+            FileAttributes attributes = File.GetAttributes(entry);
+
+            if (!attributes.HasFlag(FileAttributes.Directory))
+            {
+                if (_logger.IsEnabled(LogLevel.Error))
+                {
+                    _logger.LogError("Invalid entry type: {entry}", entry);
+                }
+
+                if (_logger.IsEnabled(LogLevel.Information))
+                {
+                    _logger.LogInformation("Moving entry to bin folder: {entry}", entry);
+                }
+
+                File.Move(entry, Path.Combine(_binPath, folderName));
+                return;
+            }
+            
+            if (!Regex.IsMatch(folderName, _folderNamePattern))
+            {
+                if (_logger.IsEnabled(LogLevel.Error))
+                {
+                    _logger.LogError("Invalid folder name: {folderName}", folderName);
+                }
+
+                if (_logger.IsEnabled(LogLevel.Information))
+                {
+                    _logger.LogInformation("Moving entry to bin folder: {entry}", entry);
+                }
+
+                Directory.Move(entry, Path.Combine(_binPath, folderName));
+                return;
+            }
+
+            string metadataPath = Path.Combine(entry, "metadata.json");
+
+            if (!File.Exists(metadataPath))
+            {
+                if (_logger.IsEnabled(LogLevel.Error))
+                {
+                    _logger.LogError("metadata.json file not found.");
+                }
+
+                if (_logger.IsEnabled(LogLevel.Information))
+                {
+                    _logger.LogInformation("Moving entry to bin folder: {entry}", entry);
+                }
+
+                Directory.Move(entry, Path.Combine(_binPath, folderName));
+                return;
+            }
+
+            if (Directory.GetFiles(entry, "*.jpg").Length == 0 && Directory.GetFiles(entry, "*.jpeg").Length == 0 && Directory.GetFiles(entry, "*.png").Length == 0)
+            {
+                if (_logger.IsEnabled(LogLevel.Error))
+                {
+                    _logger.LogError("Image file not found.");
+                }
+
+                if (_logger.IsEnabled(LogLevel.Information))
+                {
+                    _logger.LogInformation("Moving entry to bin folder: {entry}", entry);
+                }
+
+                Directory.Move(entry, Path.Combine(_binPath, folderName));
+                return;
+            }
+
+            string destination = Path.Combine(_processedPath, folderName);
+            Directory.CreateDirectory(destination);
+            File.Create(Path.Combine(destination, "results.json")).Close();
+
+            ProcessFilesAtFolder(entry, destination);
+
+            Directory.Delete(entry, false);
+        }
+
+        private void ProcessFilesAtFolder(string entry, string destination)
+        {
+            string folderName = Path.GetFileName(entry);
+
+            foreach(var file in Directory.GetFiles(entry))
+            {
+                string fileName = Path.GetFileName(file);
+                string fileExtension = Path.GetExtension(file);
+                string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(file);
+
+                if (fileName == "metadata.json")
+                {
+                    File.Move(file, Path.Combine(destination, fileName));
+                    continue;
+                }
+                else if (fileExtension == ".jpg" || fileExtension == ".jpeg" || fileExtension == ".png")
+                {
+                    // Send image to AI service
+                    // Save results to results.json file
+                    File.Move(file, Path.Combine(destination, fileName));
+                    continue;
+                }
+                else
+                {
+                    string bin = Path.Combine(_binPath, folderName);
+                    if(!Directory.Exists(bin))
+                    {
+                        Directory.CreateDirectory(bin);
+                    }
+                    File.Move(file, Path.Combine(bin, fileName));
+                }
+            }
         }
     }
 }
