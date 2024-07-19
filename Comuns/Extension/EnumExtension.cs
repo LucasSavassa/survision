0d8c0eef26d0ce7a87280b0fae0af044cc611326
Lucas Pires Savassa <lucaspsavassa@gmail.com>
2024-07-19 11:50:37 -0300

    Add enum extension methods

---
+++ Comuns/Extension/EnumExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comuns.Extension
+{
+    public static class EnumExtension
+    {
+        private static class EnumCache<T> where T : struct, Enum
+        {
+            public static readonly HashSet<T> Values = new((T[])Enum.GetValues(typeof(T)));
+        }
+
+        public static bool IsDefined<T>(this T value) where T : struct, Enum
+        {
+            return EnumCache<T>.Values.Contains(value);
+        }
+    }
+}
