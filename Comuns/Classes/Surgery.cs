4679bb081a9f151d40b0808e371e072169c4fc46
Lucas Pires Savassa <lucaspsavassa@gmail.com>
2024-10-06 13:00:23 -0300

    Implement custom date time serialization

--- Comuns/Classes/Surgery.cs
+++ Comuns/Classes/Surgery.cs
@@ -1,4 +1,5 @@
 ﻿using Comuns.Enums;
+using Comuns.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,7 @@ namespace Comuns.Classes
         public uint Shots { get; set; }
 
         [JsonPropertyName("start")]
+        [JsonConverter(typeof(CustomDateTime))]
         public DateTime Start { get; set; }
     }
 }
---
+++ Comuns/Serialization/CustomDateTime.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Globalization;
+using System.Diagnostics;
+using System.Buffers.Text;
+
+namespace Comuns.Serialization
+{
+    internal class CustomDateTime : JsonConverter<DateTime>
+    {
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            Debug.Assert(typeToConvert == typeof(DateTime));
+
+            if (DateTime.TryParseExact(reader.GetString() ?? string.Empty, "yyyy/MM/ddTHH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime value))
+            {
+                return value;
+            }
+            else
+            {
+                throw new FormatException("Unable to deserialize token to datetime because format is not 'yyyy/MM/ddTHH:mm:ss'.");
+            }
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString("yyyy/MM/ddTHH:mm:ss", CultureInfo.InvariantCulture));
+        }
+    }
+}
